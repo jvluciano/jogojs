24849bd147edadfef8a0479f3a149cfc85611b68
jvluciano <153518361+jvluciano@users.noreply.github.com>
2023-12-22 11:38:02 -0300

    Jogo versão 2.0
    Jogo interagindo pela tela e com descrição por voz.

--- app.js
+++ app.js
@@ -1,23 +1,77 @@
-alert('Bora jogar o jogo do número secreto');
-let intervaloDeNumeros = parseInt(Math.random() * 1000 + 1);
-let numeroSecreto = parseInt(Math.random() * intervaloDeNumeros +1);
+let numeroLimite = 100;
+let numerosSorteados = [];
+let numeroAleatorio = definirNumeroAleatorio();
+let contador = 1;
 let chute;
-let tentativas = 1;
-console.log (numeroSecreto);
+let mensagemDeErro;
+let mensagemDeVitória;
 
-while(chute != numeroSecreto){
-    chute = prompt(`Digite um número entre 1 e ${intervaloDeNumeros}`);
-    if(numeroSecreto == chute){
-        break;
-    } else{
-        if(chute > numeroSecreto){
-            alert(`Eroooou! o número secreto é menor que ${chute}`);
-        }else{
-            alert(`Eroooou! o número secreto é maior que ${chute}`);
-        }
-    }
-    tentativas++;
+function definirTextosExibidos(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    
 }
 
-let palavraTentativa = tentativas > 1? 'tentativas':'tentativa';
-alert(`Acertoo Miseravi! O número secreto é ${numeroSecreto}, você fez ${tentativas} ${palavraTentativa}`);
+function definirTextosIniciais(){
+    definirTextosExibidos('h1', 'Jogo do número secreto');
+    let mensagemDoIntervalo = `Digite um número de 1 a ${numeroLimite}`;
+    definirTextosExibidos('p', mensagemDoIntervalo);
+    responsiveVoice.speak('Jogo do número secreto', 'Brazilian Portuguese Female', {rate:1.2});
+}
+
+definirTextosIniciais();
+
+function verificarChute() {
+    chute = document.querySelector('input').value;
+    if (chute == numeroAleatorio) {
+        let palavraVezes = contador > 1? 'vezes':'vez';
+        definirTextosExibidos('h1', 'Acertouuu Miseravi!');
+        mensagemDeVitória = `Parabéns o número aleatório é ${numeroAleatorio}, você chutou ${contador} ${palavraVezes}`;
+        definirTextosExibidos('p', mensagemDeVitória);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        document.getElementById('chutar').setAttribute('disabled',true);
+        responsiveVoice.speak('Acertouuu Miseravi!', 'Brazilian Portuguese Female', {rate:1.2});
+    } else {
+        if (chute > numeroAleatorio) {
+            definirTextosExibidos('h1', 'Eroouuu!');
+            mensagemDeErro = `o número aleatório é menor que ${chute}`;
+            definirTextosExibidos('p', mensagemDeErro);
+            responsiveVoice.speak('Eroouuu!', 'Brazilian Portuguese Female', {rate:1.2});
+        } else {
+            definirTextosExibidos('h1', 'Eroouuu!');
+            mensagemDeErro = `o número aleatório é maior que ${chute}`;
+            definirTextosExibidos('p', mensagemDeErro);
+            responsiveVoice.speak('Eroouuu!', 'Brazilian Portuguese Female', {rate:1.2});
+        }
+    }
+    contador++;
+    limpaCampo();
+}
+
+function definirNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let tamanhoDaLista = numerosSorteados.length;
+    if(tamanhoDaLista == numeroLimite){
+        numerosSorteados = [];
+    }
+    if (numerosSorteados.includes(numeroEscolhido)){
+        return definirNumeroAleatorio();
+    }else{
+        numerosSorteados.push(numeroEscolhido);
+        return numerosSorteados[numerosSorteados.length - 1];
+    }
+}
+
+function limpaCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    limpaCampo();
+    numeroAleatorio = definirNumeroAleatorio();
+    contador = 1;
+    definirTextosIniciais();
+    document.getElementById('reiniciar').setAttribute('disabled',true);
+    document.getElementById('chutar').removeAttribute('disabled');
+}
